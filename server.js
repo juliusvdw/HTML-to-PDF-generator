6e998ce0ae3b31a49cb70039336d20d328dd45ff
Julius <juliusvdw@gmail.com>
2020-06-22 11:24:57 +0200

    Pdf generation  route created

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const express = require("express");
 
+//Bring in Workers
+const createPDF = require("./workers/puppeteerPDF.js");
+
+//Init App
 const app = express();
+
+//Create HTML to PDF Route
+//Route is created on Server file, as there is only one route
+app.get("/pdf/:websiteLink", async (req, res) => {
+  try {
+    const pdf = await createPDF();
+
+    if (!pdf || pdf === undefined) {
+      res.status(400).json({ sucess: false });
+    }
+
+    res.status(200).json({ sucess: true, pdf });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false, error });
+  }
+});
 
 app.listen(5000, console.log("Server has started on Port 5000"));
--- workers/puppeteerPDF.js
+++ workers/puppeteerPDF.js
@@ -1,10 +1,16 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
+//Init Puppeteer job
+const createPDF = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://devslist.co");
-  await page.screenshot({ path: "example.png" });
+  await page.goto("https://github.com/juliusvdw", {
+    waitUntil: "networkidle0",
+  });
+  const pdf = await page.pdf({ format: "A4" });
 
   await browser.close();
-})();
+  return pdf;
+};
+
+modules.export = createPDF;
